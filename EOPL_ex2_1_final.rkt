8d07ebea220183319df3daf54d240aadfdc5cbb4
RapidGrowth <jokergreem@gmail.com>
2016-08-12 22:22:22 +0800

    my_lang

---
+++ EOPL_ex2_1_final.rkt
@@ -0,0 +1,65 @@
+#lang racket
+
+;------------------main--------------------
+(define mul
+	(lambda (num times)
+		(cond [(belong )]
+			)))
+
+[times (zero one) (zero num)]
+
+(consult times [(zero zero)
+	            (one num)]
+	            
+	            [(add num (mul num (sub_1 times)))])
+
+(define factorial
+  (lambda (num)
+    (cond [(belong num (list zero one)) one]
+          [else (mul num
+          			 (factorial (sub_1 num)))]
+          )))
+
+
+(num ([zero one] 
+	  [one one]) 
+     
+     [mul num 
+          (factorial (sub_1 num))])
+
+
+
+(num ([(zero one) one]) 
+     
+     [mul num 
+          (factorial (sub_1 num))])
+
+num consult:{
+	zero one > one
+	two > two
+	> (self: num - 1) * num
+}
+
+(self: num -= 1, gg = 78) 
+
+(self: num - 1) ;;自動套用原值未設置參數, 智能match (num - 1) to num
+	
+
+;-----------------assist--------------------
+
+(define belong
+  (lambda (domain_point codomain)
+    (cond [(null? codomain) #f]
+          [(eq? domain_point (car codomain)) #t]
+          [else (belong domain_point (cdr codomain))]
+          )))
+
+
+(define consult
+	(lambda (domain_point codomain not_found)
+		(cond [(null? codomain) not_found]
+			  [(eq? domain_point (caar codomain)) (cadar codomain)]
+			  [else (consult domain_point (cdr codomain) not_found)]
+			)))
+
+
